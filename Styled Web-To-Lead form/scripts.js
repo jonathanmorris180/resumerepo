9116ac90a78667634af79d0bb1ddb1cc235a68fb
Jonathan Morris <musicmanmorris@gmail.com>
2020-09-25 11:32:11 -0600

    updated YLDP form

--- Styled Web-To-Lead form/scripts.js
+++ Styled Web-To-Lead form/scripts.js
@@ -1,30 +1,39 @@
+$(document).ready(function(){
+  $(".mul-select").select2({
+      placeholder: "Please select",
+      tags: true,
+  });
+});
+
+// format phone numbers
+function formatPhone(obj) {
+  var numbers = obj.value.replace(/\D/g, ''),
+  char = {0:'(',3:') ',6:' '};
+  obj.value = '';
+  for (var i = 0; i < numbers.length; i++) {
+      obj.value += (char[i]||'') + numbers[i];
+  }
+}
+
 // shows the first guardian section 
 $("#relationshipChoice").change(function() {
   if ($(this).val() == "mother") {
     $('#mother1').show();
     $('#father1').hide();
     $('#guardian1-1').hide();
-    $('#guardian2-1').hide();
   } else if ($(this).val() == "father") {
     $('#mother1').hide();
     $('#father1').show();
     $('#guardian1-1').hide();
-    $('#guardian2-1').hide();
   } else if ($(this).val() == "guardian1") {
     $('#mother1').hide();
     $('#father1').hide();
     $('#guardian1-1').show();
     $('#guardian2-1').hide();
-  } else if ($(this).val() == "guardian2") {
-    $('#mother1').hide();
-    $('#father1').hide();
-    $('#guardian1-1').hide();
-    $('#guardian2-1').show();
   } else {
     $('#mother1').hide();
     $('#father1').hide();
     $('#guardian1-1').hide();
-    $('#guardian2-1').hide();
   }
 });
 $("#relationshipChoice").trigger("change");
@@ -35,27 +44,18 @@ $("#addParentOrGuardian").change(function() {
     $('#mother2').show();
     $('#father2').hide();
     $('#guardian1-2').hide();
-    $('#guardian2-2').hide();
   } else if ($(this).val() == "father") {
     $('#mother2').hide();
     $('#father2').show();
     $('#guardian1-2').hide();
-    $('#guardian2-2').hide();
   } else if ($(this).val() == "guardian1") {
     $('#mother2').hide();
     $('#father2').hide();
     $('#guardian1-2').show();
-    $('#guardian2-2').hide();
-  } else if ($(this).val() == "guardian2") {
-    $('#mother2').hide();
-    $('#father2').hide();
-    $('#guardian1-2').hide();
-    $('#guardian2-2').show();
   } else {
     $('#mother2').hide();
     $('#father2').hide();
     $('#guardian1-2').hide();
-    $('#guardian2-2').hide();
   }
 });
 $("#addParentOrGuardian").trigger("change");
@@ -66,27 +66,18 @@ $("#relationshipChoice").change(function() {
     $("#addParentOrGuardian option:nth-child(2)").hide();
     $("#addParentOrGuardian option:nth-child(3)").show();
     $("#addParentOrGuardian option:nth-child(4)").show();
-    $("#addParentOrGuardian option:nth-child(5)").show();
   } else if ($(this).val() == "father") {
     $("#addParentOrGuardian option:nth-child(3)").hide();
     $("#addParentOrGuardian option:nth-child(2)").show();
     $("#addParentOrGuardian option:nth-child(4)").show();
-    $("#addParentOrGuardian option:nth-child(5)").show();
   } else if ($(this).val() == "guardian1") {
     $("#addParentOrGuardian option:nth-child(4)").hide();
     $("#addParentOrGuardian option:nth-child(2)").show();
     $("#addParentOrGuardian option:nth-child(3)").show();
-    $("#addParentOrGuardian option:nth-child(5)").show();
-  } else if ($(this).val() == "guardian2") {
-    $("#addParentOrGuardian option:nth-child(5)").hide();
-    $("#addParentOrGuardian option:nth-child(2)").show();
-    $("#addParentOrGuardian option:nth-child(3)").show();
-    $("#addParentOrGuardian option:nth-child(4)").show();
   } else {
     $("#addParentOrGuardian option:nth-child(2)").show();
     $("#addParentOrGuardian option:nth-child(3)").show();
     $("#addParentOrGuardian option:nth-child(4)").show();
-    $("#addParentOrGuardian option:nth-child(5)").show();
   }
 });
 $("#relationshipChoice").trigger("change");
@@ -97,27 +88,18 @@ $("#addParentOrGuardian").change(function() {
     $("#relationshipChoice option:nth-child(2)").hide();
     $("#relationshipChoice option:nth-child(3)").show();
     $("#relationshipChoice option:nth-child(4)").show();
-    $("#relationshipChoice option:nth-child(5)").show();
   } else if ($(this).val() == "father") {
     $("#relationshipChoice option:nth-child(3)").hide();
     $("#relationshipChoice option:nth-child(2)").show();
     $("#relationshipChoice option:nth-child(4)").show();
-    $("#relationshipChoice option:nth-child(5)").show();
   } else if ($(this).val() == "guardian1") {
     $("#relationshipChoice option:nth-child(4)").hide();
     $("#relationshipChoice option:nth-child(2)").show();
     $("#relationshipChoice option:nth-child(3)").show();
-    $("#relationshipChoice option:nth-child(5)").show();
-  } else if ($(this).val() == "guardian2") {
-    $("#relationshipChoice option:nth-child(5)").hide();
-    $("#relationshipChoice option:nth-child(2)").show();
-    $("#relationshipChoice option:nth-child(3)").show();
-    $("#relationshipChoice option:nth-child(4)").show();
   } else {
     $("#relationshipChoice option:nth-child(2)").show();
     $("#relationshipChoice option:nth-child(3)").show();
     $("#relationshipChoice option:nth-child(4)").show();
-    $("#relationshipChoice option:nth-child(5)").show();
   }
 });
 $("#addParentOrGuardian").trigger("change");
@@ -140,14 +122,18 @@ $("#addChild3").click(function() {
   $("#addChild4").toggle();
 });
 
-// make the inputs required when visible
+// make certain inputs required when visible
 $("form").change(function() {
-  $("input:visible").prop('required', true);
+  $(".required-when-visible").each(function(){
+    if($(this).is(":visible")){
+      $(this).prop('required', true);
+    }
+  });
 });
 $("form").trigger("change");
 
 // datepicker calendar
-$("#00N4S000000HM8U, #00N4S000000HM93, #00N4S000000HOsj, #00N4S000000HOtD").datepicker({
+$("#00N0r000000U5v1, #00N0r000000U5v7, #00N0r000000U5vD, #00N0r000000U5vJ").datepicker({
   changeMonth: true,
   changeYear: true,
   yearRange: "-100:+0",
@@ -157,13 +143,16 @@ $("#00N4S000000HM8U, #00N4S000000HM93, #00N4S000000HOsj, #00N4S000000HOtD").date
 
 // at least one guardian is required
 $("#submitForm").click(function(e) {
-  var checkDropdown = $("#relationshipChoice")
+  var checkDropdown = $("#relationshipChoice");
   if (checkDropdown.val() === "selectOption") {
-    document.getElementById("selectResult").innerHTML="Please select at least one option";
-    document.getElementById("selectResult").style.color="red";
+    var err = document.getElementById("selectResult");
+    err.innerHTML="Please select at least one parent/guardian";
+    err.style.color="red";
+    err.scrollIntoView();
     e.preventDefault();
   } else {
-    document.getElementById("selectResult").innerHTML="";
+    document.getElementById('selectResult').innerHTML="";
+    $('#form').submit();
   }
 });
 
@@ -177,16 +166,17 @@ function isFutureDate(idate){
 }
 
 $("#submitForm").click(function(e) {
-  var child1date = document.getElementById("00N4S000000HM8U"),
-      child2date = document.getElementById("00N4S000000HM93"),
-      child3date = document.getElementById("00N4S000000HOsj"),
-      child4date = document.getElementById("00N4S000000HOtD"),
+  var child1date = document.getElementById("00N0r000000U5v1"),
+      child2date = document.getElementById("00N0r000000U5v7"),
+      child3date = document.getElementById("00N0r000000U5vD"),
+      child4date = document.getElementById("00N0r000000U5vJ"),
       resultDiv = document.getElementById("dateWarn"),
       resultDiv2 = document.getElementById("dateWarn2"),
       resultDiv3 = document.getElementById("dateWarn3"),
       resultDiv4 = document.getElementById("dateWarn4");
   if(isFutureDate(child1date.value)){
       resultDiv.innerHTML = "Please select a date in the past";
+      resultDiv.scrollIntoView();
       resultDiv.style.color = "red";
       resultDiv2.innerHTML = "";
       resultDiv3.innerHTML = "";
@@ -194,6 +184,7 @@ $("#submitForm").click(function(e) {
       e.preventDefault();
   } else if(isFutureDate(child2date.value)) {
       resultDiv2.innerHTML = "Please select a date in the past";
+      resultDiv2.scrollIntoView();
       resultDiv2.style.color = "red";
       resultDiv.innerHTML = "";
       resultDiv3.innerHTML = "";
@@ -201,6 +192,7 @@ $("#submitForm").click(function(e) {
       e.preventDefault();
   } else if(isFutureDate(child3date.value)) {
       resultDiv3.innerHTML = "Please select a date in the past";
+      resultDiv3.scrollIntoView();
       resultDiv3.style.color = "red";
       resultDiv.innerHTML = "";
       resultDiv2.innerHTML = "";
@@ -208,6 +200,7 @@ $("#submitForm").click(function(e) {
       e.preventDefault();
   } else if(isFutureDate(child4date.value)) {
       resultDiv4.innerHTML = "Please select a date in the past";
+      resultDiv4.scrollIntoView();
       resultDiv4.style.color = "red";
       resultDiv.innerHTML = "";
       resultDiv2.innerHTML = "";
@@ -218,5 +211,6 @@ $("#submitForm").click(function(e) {
       resultDiv2.innerHTML = "";
       resultDiv3.innerHTML = "";
       resultDiv4.innerHTML = "";
+      $('#form').submit();
   }
 });
